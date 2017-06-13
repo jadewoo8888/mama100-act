2ee942f7732f798c575fba505376efc7273a116a
jadewoo8888 <313388925@qq.com>
2017-06-13 11:16:08 +0800

    rpc服务器

--- src/main/java/com/biostime/config/RpcConfig.java
+++ src/main/java/com/biostime/config/RpcConfig.java
@@ -1,6 +1,12 @@
 package com.biostime.config;
 
+import com.biostime.coupon.rpc.RpcFactory;
 import com.mama100.common.finagle.FinagleClientRegister;
+import com.mama100.merchandise.enums.FromSystem;
+import com.mama100.order.rpc.OrderRpcJavaFactory;
+import com.mama100.rpc.merchandise.MerchandiseRpcFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -18,5 +24,53 @@ public class RpcConfig {
 		f.setHostConnectionLimit(200);//本客户端向每个host可发出的最大请求数
 		f.setHostConnectionCoresize(50);//缓存保留的连接数
 		return f;
+	}
+
+	/**
+	 * 商品库RPC注册
+	 * @return
+	 */
+	@Bean
+	@ConfigurationProperties(prefix = "rpc.zookeeper.sku")
+	public FinagleClientRegister skuRegister(){
+		return finagleClientRegister();
+	}
+	@Bean
+	public MerchandiseRpcFactory merchandiseRpcFactory(){
+		MerchandiseRpcFactory merchandiseRpcFactory = new MerchandiseRpcFactory();
+		merchandiseRpcFactory.setRegister(skuRegister());
+		merchandiseRpcFactory.setSystemCode(FromSystem.MAMA_MOB);
+		return merchandiseRpcFactory;
+	}
+
+	/**
+	 * 优惠券RPC注册
+	 * @return
+	 */
+	@Bean
+	@ConfigurationProperties(prefix = "rpc.zookeeper.coupon")
+	public FinagleClientRegister couponRegister(){
+		return finagleClientRegister();
+	}
+	@Bean
+	public RpcFactory couponRpcFactory(){
+		RpcFactory couponRpcFactory = new RpcFactory();
+		couponRpcFactory.setFinagleClientRegister(couponRegister());
+		return couponRpcFactory;
+	}
+	/**
+	 * 订单RPC注册
+	 * @return
+	 */
+	@Bean
+	@ConfigurationProperties(prefix = "rpc.zookeeper.order")
+	public FinagleClientRegister orderRegister(){
+		return finagleClientRegister();
+	}
+	@Bean
+	public OrderRpcJavaFactory orderRpcJavaFactory(){
+		OrderRpcJavaFactory factory = new OrderRpcJavaFactory();
+		factory.setFinagleClientRegister(orderRegister());
+		return factory;
 	}
 }
