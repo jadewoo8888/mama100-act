6f47bffcf9bd1417ff8fffbd461bc92f79cbf063
jadewoo8888 <313388925@qq.com>
2017-06-22 16:17:14 +0800

    druid和跨域配置

---
+++ src/main/java/com/biostime/config/database/DruidConfig.java
@@ -0,0 +1,40 @@
+package com.biostime.config.database;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * DruidConfig
+ *
+ * @author 12465
+ * @version 1.0
+ * @createDate 2016/7/18 10:04
+ */
+@Configuration
+public class DruidConfig {
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean reg = new ServletRegistrationBean();
+        reg.setServlet(new StatViewServlet());
+        reg.addUrlMappings("/druid/*");
+        //reg.addInitParameter("allow", "127.0.0.1");
+        //reg.addInitParameter("deny","");
+        reg.addInitParameter("loginUsername", "biostime");
+        reg.addInitParameter("loginPassword", "biostime");
+        return reg;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
